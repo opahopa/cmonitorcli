b9f7a90647523e6e2e361dd049309a1ab479a747
opahopa <opahopa@gmail.com>
2018-09-13 19:03:18 -0300

    change bundle dir

--- services/monitor_functions.py
+++ services/monitor_functions.py
@@ -1,23 +1,34 @@
 import logging
-import os
+import os, sys
 
 from services.system.system_service import SystemService
 from settings.config import bundle_dir
 
 logger = logging.getLogger(__name__)
 
+if getattr(sys, 'freeze', False):
+    # running as bundle (aka frozen)
+    bundle_dir = sys._MEIPASS
+else:
+    # running live
+    bundle_dir = os.path.dirname(os.path.abspath(__file__))
+
 
 def bash_cmd_result(result):
-    if hasattr(result, 'stdout') and result.stdout and result.returncode == 0:
-        return {'success': True, 'body': result.stdout.strip()}
-    if hasattr(result, 'stderr') and result.stderr:
-        if result.stdout:
-            err = '\n' + result.stdout + '\n' + result.stderr
+    try:
+        if hasattr(result, 'stdout') and result.stdout and result.returncode == 0:
+            return {'success': True, 'body': result.stdout.strip()}
+        if hasattr(result, 'stderr') and result.stderr:
+            if result.stdout:
+                err = '\n' + result.stdout + '\n' + result.stderr
+            else:
+                err = result.stderr
         else:
-            err = result.stderr
-    else:
-        err = result
-    return {'success': False, 'body': f"Run bash script command execution error. {err}"}
+            err = result
+        return {'success': False, 'body': f"Run bash script command execution error. {err}"}
+    except Exception as e:
+        logger.error(f'bash script cmd result parsing error: {e}')
+        return {'success': False, 'body': f"bash script cmd result parsing error: {err}"}
 
 
 """"""""""""""""""""""""""""""""""""""""
